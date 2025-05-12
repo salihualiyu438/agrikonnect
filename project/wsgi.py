6e191974f7aaafe23903366e626ba4d88365f93f
salihualiyu438 <salihualiyu438@gmail.com>
2025-05-13 00:27:17 +0100

    Update wsgi.py

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/agrikonnect/project'
+if path not in sys.path:
+  sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
